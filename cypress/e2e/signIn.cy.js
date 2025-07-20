6f9d8d58374a19ecbc258302a46ae08377eb941f
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-07-20 17:44:13 +0200

    Update signIn.cy.js

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
+describe('Sign in page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://conduit.mate.academy/');
 
+    cy.contains('Sign in')
+      .click();
+
+    cy.url().should('include', '/login');
+
+    cy.get('[placeholder="Email"]')
+      .type('test@test.test');
+    cy.get('[placeholder="Password"]')
+      .type('test');
+
+    cy.contains('[type="submit"]', "Sign in")
+      .click();
+
+    cy.get('nav').contains('testtestowy')
+      .should('be.visible');
   });
 });
